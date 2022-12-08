e98261a01ee97441b0352dd24a3953cd692ed7d5
Biruk-sew <mbiruk1020@gmail.com>
2022-12-08 07:13:14 -0800

    remaing tasks

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,5 +15,5 @@ int main(void)
 	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %lu byte(s)\n" sizeof(long long int));
 	printf(:Size of a float: %lu byte(s)\n", sizeof(float));
-	retturn (0);
+	return (0);
 }
