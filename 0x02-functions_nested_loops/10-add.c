cfb376a606edddf3e299d8ede24c6b9f514d8ce0
Biruk-sew <mbiruk1020@gmail.com>
2022-12-13 06:36:49 -0800

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - add two numbers
+ * @a: first numbers
+ * @b: second numbers
+ * Return: add both numbers
+ */
+int add(int a, int b)
+{
+	return (a, b);
+}
---
+++ 10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - add two numbers
+ * @a: first number
+ * @b:second number
+ * Return: add both number
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
