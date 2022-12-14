e0f1cc3f58cde866315ecc1a6c626bd1d65d3096
Biruk-sew <mbiruk1020@gmail.com>
2022-12-14 06:31:52 -0800

    task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,6 +1,5 @@
 #include "main.h"
 /**
- *
  * main - test the function positive or negative
  * Return: 0
  */
@@ -9,7 +8,7 @@ int main(void)
 	int i;
 
 	i = 0;
-	postitive_or_negative(i);
+	positive_or_negative(int i);
 
 	return (0);
 }
