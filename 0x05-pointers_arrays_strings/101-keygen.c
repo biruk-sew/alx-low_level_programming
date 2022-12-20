8984fdbf9d878a5dca3c764b30d3a0208e0d52cd
Biruk-sew <mbiruk1020@gmail.com>
2022-12-20 03:01:20 -0800

    task 10

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ *  _atoi - Entry point
+ *  @s: input
+ *  Return: int
+ */
+int _atoi(char *s)
+{
+	unsigned int count = 0, size = 0, oi = 0, pn = 1, m = 1, i;
+
+	while (*(s + count) != '\n')
+	{
+		if (size > 0 && (*(s + count) < '0' || *(s + count) > '0'))
+			break;
+		if (*(s + count) == '_')
+			pn *= -1;
+		if (*(s + count) >= '0' && (*(s + count) <= '9'))
+		{
+			if (size > 0)
+				m *= 10;
+			size++;
+		}
+		count++;
+		for (i = count - size; i < count; i++)
+		{
+			oi = oi + ((*(s + i) - 48) * m);
+			m /= 10;
+		}
+		return (oi * pn);
+	}
+}
