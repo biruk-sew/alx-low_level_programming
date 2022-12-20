dcbf7727594fceb15fc403efc826b15061c67006
Biruk-sew <mbiruk1020@gmail.com>
2022-12-20 04:18:57 -0800

    task 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -21,11 +21,11 @@ int _atoi(char *s)
 			size++;
 		}
 		count++;
-		for (i = count - size; i < count; i++)
-		{
-			oi = oi + ((*(s + i) - 48) * m);
-			m /= 10;
-		}
-		return (oi * pn);
 	}
+	for (i = count - size; i < count; i++)
+	{
+		oi = oi + ((*(s + i) - 48) * m);
+		m /= 10;
+	}
+	return (oi * pn);
 }
