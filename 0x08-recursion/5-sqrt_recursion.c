a111a2d3115c91cbf70f3cb27f49fdc19da10eaf
Biruk-sew <mbiruk1020@gmail.com>
2023-01-04 12:21:41 -0800

    tasks

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -10,7 +10,7 @@ int _evaluate(int i, int n)
 	if (n == 0 || n == 1)
 		return (n);
 	else if (i * i < n)
-		return (_evaluate(i + n, n));
+		return (_evaluate(i + 1, n));
 	else if (i * i == n)
 		return (i);
 	return (-1);
