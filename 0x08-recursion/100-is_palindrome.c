7a2b4df28125f19f70ef7999824542083ba41131
Biruk-sew <mbiruk1020@gmail.com>
2023-01-05 01:23:52 -0800

    tasks

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * _strlen_recursion - Entery point
+ * @s: input
+ * Return: recursion
+ */
+int _strlen_recursion(char *s)
+{
+	if (!*s)
+	{
+		return (0);
+	}
+	return (1 + _strlen_recursion(++s));
+}
+/**
+ * p1 - Entery point
+ * @s: input
+ * @l: input
+ * Return: boolean
+ */
+int p1(char *s, int l)
+{
+	if (l < 1)
+	{
+		return (1);
+	}
+	if (*s == *(s + l))
+	{
+		return (p1(s + 1, l - 2));
+	}
+	return (0);
+}
