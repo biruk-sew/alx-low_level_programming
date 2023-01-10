91ba637b0b4c0820df3c39cfeb5ab28c69783633
Biruk-sew <mbiruk1020@gmail.com>
2023-01-10 12:58:13 -0800

    task 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -23,7 +23,7 @@ int **alloc_grid(int width, int height)
 	}
 	for (i = 0; i < height; i++)
 	{
-		grid[i] = malloc(sizeof(int *) * width);
+		grid[i] = malloc(sizeof(int) * width);
 		if (grid[i] == NULL)
 		{
 			for (i = i - 1; i >= 0; i--)
