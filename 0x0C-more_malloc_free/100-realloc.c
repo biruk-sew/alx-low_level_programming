3bb223b6b35ec39ed2c1b15db5ef8609b0eb6e4a
Biruk-sew <mbiruk1020@gmail.com>
2023-01-12 11:46:03 -0800

    task

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -25,7 +25,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (0);
 	}
 	relloc = malloc(new_size);
-	if (rlloc == NULL)
+	if (relloc == NULL)
 		return (0);
 	for (i = 0; i < (old_size || i < new_size); i++)
 		*(relloc + i) = clone[i];
