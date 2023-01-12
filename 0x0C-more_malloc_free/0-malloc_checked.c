8ea386329ff030c8956a570c96b01ea1e96fa7ba
Biruk-sew <mbiruk1020@gmail.com>
2023-01-12 09:21:27 -0800

    task

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#inclukde "main.h"
+/**
+ * malloc_checked - Entery point
+ * @b: input
+ * Return: void
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *p;
+
+	p = malloc(b);
+	if (p == NULL)
+	{
+		exit(98);
+	}
+	return (p);
+}
